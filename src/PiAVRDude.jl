49174db95fd14fefd91109ab5f6597a989246ca7
notinaboat <notinaboat@icloud.com>
2021-02-09 14:08:44 +1100

    add flashing functions

--- src/PiAVRDude.jl
+++ src/PiAVRDude.jl
@@ -5,12 +5,18 @@ AVRDude interface for Raspberry Pi GPIO ISP.
 """
 module PiAVRDude
 
+export AVRDude
+
 struct AVRDude
+
     name::String
+    device::String
     config::String
-    function AVRDude(name;
-                     sck=nothing, miso=nothing, mosi=nothing, reset=nothing)
-        new(name, """
+
+    function AVRDude(;device=nothing,
+                      sck=nothing, miso=nothing, mosi=nothing, reset=nothing)
+        name = "gpio$reset"
+        new(name, device, """
             programmer
               id    = "$name";
               desc  = "GPIO ISP";
@@ -24,7 +30,31 @@ struct AVRDude
     end
 end
 
+
 config(isp) = isp.config
+
+
+function avrdude(isp, cmd)
+    mktempdir() do d
+        conf=joinpath(d, "avrdude.conf")
+        write(conf, config(isp))
+        options = `-p $(isp.device) -C +$conf -c $(isp.name)`
+        cmd = `avrdude $options $cmd`
+        println("Running: ", cmd)
+        run(cmd)
+    end
+end
+
+status(isp) = avrdude(isp, `-v`)
+
+flash_hex(isp, file) = avrdude(isp, `-U flash:w:$file:i`)
+flash_elf(isp, file) = avrdude(isp, `-U flash:w:$file:e`)
+flash(i, f) = endswith(f, ".elf") ? flash_elf(i, f) : flash_hex(i, f)
+
+read(isp, type, file) = avrdude(isp, `-U $type:r:$file:i`)
+
+fuse(isp, fuse) = avrdude(isp, `-U $fuse`)
+
 
 
 end # module
