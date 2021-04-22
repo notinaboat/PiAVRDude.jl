f80a356198cf21a926b666da6692d02f45a76c0a
notinaboat <notinaboat@icloud.com>
2021-04-22 15:14:51 +1000

    add eeprom programming function, disable safe-mode

--- src/PiAVRDude.jl
+++ src/PiAVRDude.jl
@@ -17,6 +17,7 @@ struct AVRDude
                       sck=nothing, miso=nothing, mosi=nothing, reset=nothing)
         name = "gpio$reset"
         new(name, device, """
+            default_safemode = no;
             programmer
               id    = "$name";
               desc  = "GPIO ISP";
@@ -49,7 +50,10 @@ status(isp) = avrdude(isp, `-v`)
 
 flash_hex(isp, file) = avrdude(isp, `-U flash:w:$file:i`)
 flash_elf(isp, file) = avrdude(isp, `-U flash:w:$file:e`)
+eeprom_hex(isp, file) = avrdude(isp, `-U eeprom:w:$file:i`)
+eeprom_elf(isp, file) = avrdude(isp, `-U eeprom:w:$file:e`)
 flash(i, f) = endswith(f, ".elf") ? flash_elf(i, f) : flash_hex(i, f)
+eeprom(i, f) = endswith(f, ".elf") ? eeprom_elf(i, f) : eeprom_hex(i, f)
 
 read(isp, type, file) = avrdude(isp, `-U $type:r:$file:i`)
 
